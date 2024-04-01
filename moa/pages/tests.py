cfcb5547c929b32e85c86bf4ed26e6a49ae081aa
Jane Im <alwaysjane15@gmail.com>
2024-04-01 01:00:43 -0400

    tests for templates app (page)

--- moa/pages/tests.py
+++ moa/pages/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 # Create your tests here.
 class HomepageTests(SimpleTestCase):
@@ -14,3 +16,11 @@ class HomepageTests(SimpleTestCase):
 	def test_homepage_template(self):
 		response = self.client.get("/")
 		self.assertTemplateUsed(response, "home.html")
+
+	def test_homepage_does_not_contain_incorrect_html(self):
+		response = self.client.get("/")
+		self.assertNotContains(response, "Hi there! I should not be on this page.")
+
+	def test_homepage_url_resolves_homepageview(self):
+		view = resolve("/")
+		self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
