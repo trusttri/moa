41d26988d9db6f2fedc1cbcf59d3e6f423f1a6da
Jane Im <alwaysjane15@gmail.com>
2024-04-05 14:25:52 -0400

    first draft of experience

--- moa/core/models.py
+++ moa/core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.db.models import IntegerField, CharField, EmailField, TextField
 import uuid
 
 # Create your models here.
-class User(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	stored_at = models.DateTimeField(null=False)
-	email = models.EmailField(max_length=254)
-	password = models.TextField()
+class Experience(models.Model):
+	title = models.CharField(max_length=200)
+	author = models.ForeignKey(
+				get_user_model(),
+				on_delete=models.CASCADE,
+		)
+	def __str__(self):
+		return self.title
