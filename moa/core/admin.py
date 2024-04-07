2b24810db7db07e288b6e15bb17d1985b2e900b5
Jane Im <alwaysjane15@gmail.com>
2024-04-07 13:04:30 -0400

    tag section in admin page

--- moa/core/admin.py
+++ moa/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Experience
+from .models import Experience, Tag
 
 # Register your models here.
 class ExperienceInline(admin.TabularInline):
@@ -9,3 +9,11 @@ class ExperienceAdmin(admin.ModelAdmin):
 	list_display = ("title", "author")
 
 admin.site.register(Experience, ExperienceAdmin)
+
+class TagInline(admin.TabularInline):
+	model = Tag
+
+class TagAdmin(admin.ModelAdmin):
+	list_display = ("keyword", "explanation")
+
+admin.site.register(Tag, TagAdmin)
