65a5e7734204ef300403b274a3c70b589b1b888e
Jane Im <alwaysjane15@gmail.com>
2024-05-30 16:34:06 -0400

    uuid

--- moa/core/models.py
+++ moa/core/models.py
@@ -14,6 +14,7 @@ class Tag(models.Model):
 		return self.keyword
 		
 class Note(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4)
 	author = models.ForeignKey(
 				get_user_model(),
 				on_delete=models.CASCADE,
@@ -27,11 +28,10 @@ class Note(models.Model):
 	first_gen = models.BooleanField(null=True, blank=True)
 	other_info = models.TextField(null=True, blank=True)
 
-	reference = models.ManyToManyField('Note') 
+	reference = models.ManyToManyField('Note')
 
 	def __str__(self):
 		return self.text
 
 	# def get_absolute_url(self):
 	# 	return reverse("experience", args=[str(self.id)])
-
