6ea51d4d630637cf564e64baaf92519139b21944
Jane Im <alwaysjane15@gmail.com>
2024-05-31 12:26:58 -0400

    set up channels and daphne

--- moa/moa/settings.py
+++ moa/moa/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -97,7 +98,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'moa.wsgi.application'
-
+ASGI_APPLICATION = 'moa.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
